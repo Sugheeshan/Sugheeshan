deb614ae78d787fb61ab6a50de7575a0a7ce800a
Sugheeshan <88663444+Sugheeshan@users.noreply.github.com>
2022-03-15 11:49:48 +0530

    Add files via upload

---
+++ P138-EXAMPLE FOR TOUPPER.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdio.h>
+void main()
+{
+char ch;
+clrscr();
+cout<<"\nEnter a character";
+ch=getchar();
+ch=toupper(ch);
+cout<<"\nThe given character is coverted to uppercase letter ";
+getch();
+}
